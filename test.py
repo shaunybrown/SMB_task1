763ce9622f10d59226288d3d8afd1e1470f3703b
shaunybrown <shaun_brown_@hotmail.co.uk>
2019-10-21 08:17:42 +0100

    Add notes

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import urllib.request
 import pandas as pd
 
-results = urllib.request.urlopen("http://127.0.0.1:8000/clinicalAnalysis/").read()
-s = str(results,'utf-8')
-list = s[1:-1].split(",")
-df=pd.DataFrame(list).rename(columns={0: "Predictions"})
+results = urllib.request.urlopen("http://127.0.0.1:8000/clinicalAnalysis/").read() #GET API results
+s = str(results,'utf-8') #CONVERT RESPONSE TO A STRING
+list = s[1:-1].split(",") #DROP "[" AND "]" & CONVERT TO LIST
+df=pd.DataFrame(list).rename(columns={0: "Predictions"}) #CONVERT TO PANDAS DATAFRAME
 
 print (df)
