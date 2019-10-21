3ca78aa1ce1341da93fbf5fb1bf3ac4a058b4b42
shaunybrown <shaun_brown_@hotmail.co.uk>
2019-10-21 08:19:39 +0100

    add notes

--- clinicalAnalysis/views.py
+++ clinicalAnalysis/views.py
@@ -6,8 +6,8 @@ from django.http import JsonResponse
 def index(request):
     csv = pd.read_csv('docs/source/test_data_for_candidate_python.csv')
     X = csv.drop(["Has_Fancy_Title","Name_Length","Survived", "Predictions"],axis=1).astype({"Pclass":"int", "Sex":"bool", "Age":"int", "SibSp":"int", "Parch":"int", "Fare":"float", "Embarked_S": "bool", "Embarked_C":"bool"})
+    #Above: drop unneeded columns and set column types
     model = sklearn.externals.joblib.load('docs/source/model_python.pkl')
     predictions = model.predict_proba(X)
     results = predictions[:,1].tolist()
-    print(predictions[:,1])
     return JsonResponse(results, safe=False)
